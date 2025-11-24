61051f071b0c5017b225327fd738b5e50993be65
Andor Gunczer <80578738+AndorGunczer@users.noreply.github.com>
2025-11-24 20:33:49 +0100

    conflict resolve: error-log.md => pushed by mistake

--- CICD.sh
+++ CICD.sh
@@ -1,12 +1,12 @@
-#!/bin/bash
+# #!/bin/bash
 
-set -e
+# set -e
 
-APP_NAME=my_store
-VERSION=INSECURE
+# APP_NAME=my_store
+# VERSION=INSECURE
 
-#BUILD
-docker compose -f ./infra/docker/docker-compose.yaml -p my_store up --detach
-npx nx run-many --target=build
+# #BUILD
+# docker compose -f ./infra/docker/docker-compose.yaml -p my_store up --detach
+# npx nx run-many --target=build
 
-#TEST
+# #TEST
